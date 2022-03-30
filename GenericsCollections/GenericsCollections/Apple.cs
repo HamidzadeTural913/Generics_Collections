095cdc43c60237ce90b73986b569ca98c607f078
HamidzadeTural913 <tu76fe6rc@code.edu.az>
2022-03-30 14:05:30 +0400

    Task Added

---
+++ GenericsCollections/GenericsCollections/Apple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsCollections
+{
+    internal class Apple
+    {
+        public string Vitamin;
+        public Apple(string vitamin)
+        {
+            Vitamin = vitamin;
+
+
+        }
+        public override string ToString()
+        {
+            return $"Vitamin:{Vitamin}";
+        }
+    }
+}
---
+++ GenericsCollections/GenericsCollections/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace GenericsCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            Lemon lemon = new Lemon("Vitamin-C");
+            Pineapple lemonApple = new Pineapple("Vitamin-B");
+            Apple apple = new Apple("Vitamin-A");
+
+            Console.WriteLine("LemonList:");
+            BasketList<Lemon> basketList = new BasketList<Lemon>();
+            basketList.AddItem(lemon);
+            foreach (var item in basketList.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("PineappleList:");
+
+            BasketList<Pineapple> basketList2 = new BasketList<Pineapple>();
+            basketList2.AddItem(lemonApple);
+            foreach (var item in basketList2.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("AppleList:");
+            BasketList<Apple> basketList3 = new BasketList<Apple>();
+            basketList3.AddItem(apple);
+            foreach (var item in basketList3.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+
+            
+                SortedList sortedList = new SortedList();
+
+                sortedList.Add(2001, "C#");
+                sortedList.Add(1990, "Python");
+                sortedList.Add(1995, "JavaScript");
+                sortedList.Add(1996, "Java");
+
+                Console.WriteLine("Key is sorting:");
+                foreach (DictionaryEntry item in sortedList)
+                {
+                    Console.WriteLine(item.Key);
+                }
+
+
+                Console.WriteLine("Value is sorting:");
+                foreach (DictionaryEntry item in sortedList)
+                {
+                    Console.WriteLine(item.Value);
+                }
+
+            
+        }
+    }
+}
